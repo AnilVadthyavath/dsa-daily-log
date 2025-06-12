b63f673f931ba3f1abef567feae5782433c61350
Vadthyavath Anil <vadthyavathanil.ai@gmail.com>
2025-06-12 18:10:43 +0530

    Update Factorial-of-a-number.cpp

--- Mathematics/003-Factorial-of-a-number/Factorial-of-a-number.cpp
+++ Mathematics/003-Factorial-of-a-number/Factorial-of-a-number.cpp
@@ -1,1 +1,22 @@
+# native solution
 
+class Solution {
+public:
+    int factorial(int n) {
+        int res = 1;
+        for (int i = 2; i <= n; i++)
+            res = res * i;
+        return res;
+    }
+};
+
+# optimized solution
+
+class Solution {
+public:
+    int factorial(int n) {
+        if (n == 0)
+            return 1;
+        return n * factorial(n - 1);
+    }
+};
