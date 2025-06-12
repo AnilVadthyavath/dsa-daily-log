5cdd61a096384ee2dea4f5f93dffd395cdae42ae
Vadthyavath Anil <vadthyavathanil.ai@gmail.com>
2025-06-12 19:43:17 +0530

    Update LCM-of-Two-Numbers.cpp

--- Mathematics/006-LCM-of-Two-Numbers/LCM-of-Two-Numbers.cpp
+++ Mathematics/006-LCM-of-Two-Numbers/LCM-of-Two-Numbers.cpp
@@ -1,1 +1,29 @@
+#Brute-Force 
+
+class Solution {
+public:
+    int lcm(int a, int b) {
+        int res = std::max(a, b);
+        while (true) {
+            if (res % a == 0 && res % b == 0)
+                return res;
+            res++;
+        }
+    }
+};
+
+
+# optimzed
+
+class Solution {
+public:
+    int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+};
 
