faaa3578c23ec15f87b82238cd62d2252b7ccb59
Vadthyavath Anil <vadthyavathanil.ai@gmail.com>
2025-06-12 17:30:34 +0530

    Update Count-digits.cpp

--- Mathematics/001-Count-digits/Count-digits.cpp
+++ Mathematics/001-Count-digits/Count-digits.cpp
@@ -1,1 +1,13 @@
+class Solution {
+public:
+    int countDigits(int x) {
+        int count = 0;
 
+        while (x > 0) {
+            x = x / 10;
+            count++;
+        }
+
+        return count;
+    }
+};
