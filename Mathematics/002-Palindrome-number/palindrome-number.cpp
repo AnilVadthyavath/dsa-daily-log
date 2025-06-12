b69927dc867a7dfbccc9187834aa4aab96cf0e16
Vadthyavath Anil <vadthyavathanil.ai@gmail.com>
2025-06-12 17:44:36 +0530

    Update palindrome-number.cpp

--- Mathematics/002-Palindrome-number/palindrome-number.cpp
+++ Mathematics/002-Palindrome-number/palindrome-number.cpp
@@ -1,1 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int n) {
+        int rev = 0;
+        int temp = n;
 
+        while (temp != 0) {
+            int last_digit = temp % 10;
+            rev = rev * 10 + last_digit;
+            temp = temp / 10;
+        }
+
+        return (rev == n);
+    }
+};
