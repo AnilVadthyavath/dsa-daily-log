df12786e7e43c65fb1d9fe1a8cee3c46bd7eaf21
Vadthyavath Anil <vadthyavathanil.ai@gmail.com>
2025-06-12 19:25:40 +0530

    Update GCD-or-HCF-of-two-Numbers.cpp

--- Mathematics/005-GCD-or-HCF-of-two-Numbers/GCD-or-HCF-of-two-Numbers.cpp
+++ Mathematics/005-GCD-or-HCF-of-two-Numbers/GCD-or-HCF-of-two-Numbers.cpp
@@ -1,1 +1,41 @@
+# Brute force:
 
+class Solution {
+public:
+    int gcd(int a, int b) {
+        int res = std::min(a, b);
+        while (res > 0) {
+            if (a % res == 0 && b % res == 0) {
+                break;
+            }
+            res--;
+        }
+        return res;
+    }
+};
+
+# Euclidean Algorithm (More Efficient):
+
+class Solution {
+public:
+    int gcd(int a, int b) {
+        while (a != b) {
+            if (a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+        return a;
+    }
+};
+
+# optimized
+class Solution {
+public:
+    int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        else
+            return gcd(b, a % b);
+    }
+};
