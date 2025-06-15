dc5afc0a807e79156b3a31632f963b92215438ef
Vadthyavath Anil <vadthyavathanil.ai@gmail.com>
2025-06-15 14:38:24 +0530

    Update Sieve-of-Eratosthenes.cpp

--- Mathematics/010-Sieve-of-Eratosthenes/Sieve-of-Eratosthenes.cpp
+++ Mathematics/010-Sieve-of-Eratosthenes/Sieve-of-Eratosthenes.cpp
@@ -1,1 +1,74 @@
+#naive solution
+class Solution {
+public:
+    vector<int> printPrimes(int n) {
+        vector<int> primes;
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                primes.push_back(i);
+            }
+        }
+        return primes;
+    }
+
+private:
+    bool isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
+    }
+};
+
+#Sieve of Eratosthenes code
+class Solution {
+public:
+    vector<int> sieve(int n) {
+        vector<bool> isPrime(n + 1, true);
+        vector<int> primes;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = 2 * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i]) {
+                primes.push_back(i);
+            }
+        }
+
+        return primes;
+    }
+};
+
+#optimized Sieve of Eratosthenes
+class Solution {
+public:
+    vector<int> sieve(int n) {
+        vector<bool> isPrime(n + 1, true);
+        vector<int> primes;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i]) {
+                primes.push_back(i);
+            }
+        }
+
+        return primes;
+    }
+};
 
