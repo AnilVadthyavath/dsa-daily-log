4c25a000a273426e702b6a7c492c137502ad401e
Vadthyavath Anil <vadthyavathanil.ai@gmail.com>
2025-06-18 22:12:33 +0530

    Update Iterative-Power.cpp

--- Mathematics/013-Iterative-Power/Iterative-Power.cpp
+++ Mathematics/013-Iterative-Power/Iterative-Power.cpp
@@ -1,1 +1,15 @@
+class Solution {
+public:
+    int power(int x, int n) {
+        int result = 1;
+        while (n > 0) {
+            if (n % 2 != 0)
+                result *= x;
+            x *= x;
+            n /= 2;
+        }
+        return result;
+    }
+};
+
 
